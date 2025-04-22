ccf15aafcffc15dbc41986ca396d5ba5ec370a4f
DoogieMac <xjoelee@gmail.com>
2025-04-22 11:23:45 +0900

    joeMac:250422-1123:

--- openai-app-t12/ch02/streamlit-audio-recorder-main/joe.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from st_audiorec import st_audiorec
-
-st.title('streamlit audio recorder')
-wav_audio_data = st_audiorec()
-
-if wav_audio_data is not None:
-    st.audio(wav_audio_data, format='audio/wav')
