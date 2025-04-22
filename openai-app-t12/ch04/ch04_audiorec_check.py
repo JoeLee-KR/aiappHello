f4876cb1137aafeb6817c9bcba30445b98239d33
DoogieMac <xjoelee@gmail.com>
2025-04-22 11:18:24 +0900

    joeMac:250422-1118:

---
+++ openai-app-t12/ch04/ch04_audiorec_check.py
@@ -0,0 +1,11 @@
+import sys
+import streamlit as st
+from st_audiorec import st_audiorec
+
+st.write("Python executable path:", sys.executable)
+
+st.title('streamlit audio recorder')
+wav_audio_data = st_audiorec()
+
+if wav_audio_data is not None:
+    st.audio(wav_audio_data, format='audio/wav')
